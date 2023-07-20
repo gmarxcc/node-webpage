6245d034f79de297caf3f2fd7c17d7604e400ed0
Gerardo Marx Chávez-Campos <gmarx_cc@itmorelia.edu.mx>
2023-07-19 23:43:42 -0600

    video: until go to a cafe

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
   return (
-	<div>Hello World</div>
-  )	
+	<Container>
+	<Box borderRadius='lg' bg='red' mb={9} p={3.5} align='center'> 
+	Hello, I&apos;m a noob developer!!
+	</Box>
+
+	<Box display={{ md: 'flex' }}>
+		<Box flexGrow={2}>
+			<Heading as='h2' variant='page-title'>
+				Gerardo Marx
+			</Heading>
+		<p> About me ( Arts / Developer / Designer )</p>
+		</Box>
+	</Box>
+	</Container>
+  )
 }
 
 export default Page
